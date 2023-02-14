8da712109454d05e93ffbfd43b02fc6d0b7c9b68
Kexin CHEN <coco@Cocos-MacBook-Pro.local>
2023-02-14 20:33:50 +0100

    changes on testing branch

---
+++ maketable.sh
@@ -0,0 +1,5 @@
+cd ~/gasprices   # make sure we are in the right place
+cut -d ';' -f 5 carburants.csv | tr [:lower:] [:upper:] | sort | uniq -c | 
+sort
+# totally experimental feature
+echo my test file.
